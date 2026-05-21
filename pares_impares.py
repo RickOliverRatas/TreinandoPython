2097a91d46c07f3667bc4fc045467a9a796d2347
unknown <rickdeoliveira45@gmail.com>
2026-05-21 18:08:50 -0300

    for while

---
+++ pares_impares.py
@@ -0,0 +1,11 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print('Pares:')
+for num in numeros:
+    if num % 2 == 0:
+        print(num)
+
+print("Impares: ")
+for num in numeros:
+    if num % 2 != 0:
+        print(num)
